eeca344004bb66861e713b7ddfc21217ff6b7be8
hoangvythptlhp0710 <hoangvytb7891@gmail.com>
2023-04-09 10:56:42 +0700

    update order status

--- src/main/java/com/example/ecommerce/repository/PromotionRepository.java
+++ src/main/java/com/example/ecommerce/repository/PromotionRepository.java
@@ -4,9 +4,11 @@ import com.example.ecommerce.domain.Promotion;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
     Page<Promotion> findAllByIsUsed(boolean isUsed, Pageable pageable);
 
--- src/main/java/com/example/ecommerce/repository/ReviewRepository.java
+++ src/main/java/com/example/ecommerce/repository/ReviewRepository.java
@@ -4,9 +4,11 @@ import com.example.ecommerce.domain.Product;
 import com.example.ecommerce.domain.Review;
 import com.example.ecommerce.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     List<Review> findAllByProduct(Product product);
