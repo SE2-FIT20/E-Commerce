09753a967b3669f453ae10cae913cb4241c0ab5c
HaiNguyen <bongnghin5@gmail.com>
2023-04-27 13:49:21 +0700

    fix getting coupons of store

--- src/main/java/com/example/ecommerce/repository/CouponSetRepository.java
+++ src/main/java/com/example/ecommerce/repository/CouponSetRepository.java
@@ -13,9 +13,8 @@ import java.time.LocalDateTime;
 @Repository
 public interface CouponSetRepository extends JpaRepository<CouponSet, Long> {
 
-    @Query
     Page<CouponSet> findAllByStore(Store store, Pageable pageable);
 
-    @Query("select cs FROM CouponSet cs WHERE cs.id in  (select cs.id from CouponSet cs JOIN Coupon c ON cs = c.couponSet AND c.customer IS NULL  GROUP BY cs.id HAVING COUNT(*) > 0) order by cs.percent desc")
+    @Query("select cs FROM CouponSet cs WHERE cs.id in  (select cs.id from CouponSet cs JOIN Coupon c ON cs = c.couponSet AND c.customer IS NULL  GROUP BY cs.id HAVING COUNT(*) > 0) AND  cs.store = ?1 AND cs.expiredAt > CURRENTTIME() order by cs.percent desc")
     Page<CouponSet> findAllByStoreAndExpiredAtAfterOrderByPercentDesc(Store store, LocalDateTime now, Pageable pageable);
 }
